388dac276e2dea789f01c340c95839810908e5a1
Your Name <vazerius@gmail.com>
2023-06-07 15:46:50 +0300

    update

--- src/utils/authGoogle.js
+++ src/utils/authGoogle.js
@@ -9,7 +9,7 @@ const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4040/api/auth/google/callback"
+    callbackURL: "https://apartments-backend.onrender.com/api/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
       userProfile=profile;
