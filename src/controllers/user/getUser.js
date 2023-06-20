624dfef4e73f751bfe03f2b7ff2ecc63d01658d2
Oleksii Shevchenko <94850347+oleksii-she@users.noreply.github.com>
2023-06-20 16:56:16 +0300

    pdate

--- src/controllers/user/getUser.js
+++ src/controllers/user/getUser.js
@@ -4,13 +4,16 @@ const { HttpError } = require("../../helpers");
 const getUser = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const { email, name, userRating, usersRatings } = await User.findById(id);
+    const { email, name, userRating, usersRatings, phone } =
+      await User.findById(id);
 
     if (!email) {
       throw HttpError(404, `User with id:${_id} not found`);
     }
 
-    res.status(200).json({ data: { email, name, userRating, usersRatings } });
+    res
+      .status(200)
+      .json({ data: { email, name, userRating, usersRatings, phone } });
   } catch (error) {
     console.error(error);
     next(error);
